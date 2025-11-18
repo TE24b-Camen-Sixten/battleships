762597fcb81dadfc56839351018036b51367a9aa
TE24b-Camen-Sixten <sixtencamen@icloud.com>
2025-11-18 16:22:17 +0100

    first commit

---
+++ battleships/Program.cs
@@ -0,0 +1,18 @@
+﻿static void PrintBoard()
+{
+    int boardHeight = 10;
+    int boardWidth = 10;
+
+    int[,] board = new int[boardHeight, boardWidth];
+
+    for (int y = 0; y < boardHeight; y++)
+    {
+        for(int x = 0; x < boardWidth; x++)
+        {
+            Console.Write(board[y, x]);
+        }
+        Console.WriteLine();
+    }
+}
+PrintBoard();
+Console.ReadLine();
