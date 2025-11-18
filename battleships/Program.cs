e383d0ed5f3ce5e820dc29fd2816ffd96875cb21
TE24b-Camen-Sixten <sixtencamen@icloud.com>
2025-11-19 00:17:49 +0100

    gjort start och lite annat smått och gott

--- battleships/Program.cs
+++ battleships/Program.cs
@@ -1,18 +1,48 @@
-﻿static void PrintBoard()
+﻿static void Start()
 {
-    int boardHeight = 10;
-    int boardWidth = 10;
+    Console.WriteLine("Welcome to Battleships!\n");
+    
+    bool success1 = false;
+    bool success2 = false;
 
-    int[,] board = new int[boardHeight, boardWidth];
-
-    for (int y = 0; y < boardHeight; y++)
+    while(success1 == false && success2 == false)
     {
-        for(int x = 0; x < boardWidth; x++)
+        Console.WriteLine("How high do you want the board to be (10 recomended)");
+        string boardHeightString = Console.ReadLine();
+        Console.WriteLine("How wide do you want the board to be (10 recomended)");
+        string boardWidthString = Console.ReadLine();
+
+        success1 = int.TryParse(boardHeightString, out int boardHeight);
+        success2 = int.TryParse(boardWidthString, out int boardWidth);
+        if (success1 == true && success2 == true)
+        {   
+            Console.WriteLine("");
+            int[,] board = new int[boardHeight, boardWidth];
+            PrintBoard(boardHeight, boardWidth, board);
+        }
+    }
+}
+
+static void PrintBoard(int height, int width, int[,] board)
+{
+    for (int y = 0; y < height; y++)
+    {
+        for (int x = 0; x < width; x++)
         {
-            Console.Write(board[y, x]);
+            Console.Write("~ ");
         }
         Console.WriteLine();
     }
+
+    PlaceShips(board, height, width);
 }
-PrintBoard();
-Console.ReadLine();
+
+static void PlaceShips(int[,] board, int height, int width)
+{
+    Console.Write("\nVar vill du placera dina skepp?\ny:");
+    string yPlaceString = Console.ReadLine();
+    Console.Write("x:");
+    string xPlaceString = Console.ReadLine();
+}
+
+Start();
