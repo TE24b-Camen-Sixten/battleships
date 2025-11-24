172bec1e07e7559824eb8bd76d13d07b5d664429
BaBajb <sixtencamen@icloud.com>
2025-11-24 23:24:42 +0100

    Gjort allt möjligt

--- battleships/Program.cs
+++ battleships/Program.cs
@@ -67,13 +67,11 @@ static void PrintBoard(bool useBoard1, int height, int width, int[,] board1, int
                 }
             }
         }
-        Console.WriteLine();
     }
 }
 
 static void PlaceShips(bool p1, int[,] board1, int[,] board2, int height, int width, int shipsAmount, bool printBoard) //Låter användaren placera sinna skepp, kan även skriva ut brädet genom att kalla på den metoden
 {
-
     if (printBoard)// Om brädet ska skrivas, skrivs brädet ut genom att kalla på en metod soms kriver ut brädet
     {
         PrintBoard(p1, height, width, board1, board2);
@@ -123,25 +121,77 @@ static void PlaceShips(bool p1, int[,] board1, int[,] board2, int height, int wi
 
 static void Attack(bool p1, int[,] board1, int[,] board2)
 {
-    if (p1)
+    bool successY = false;
+    bool successX = false;
+    while (successY && successX)
     {
         Console.Write("Choose which square to attack\ny:");
         string yPlaceString = Console.ReadLine();
         Console.Write("x:");
         string xPlaceString = Console.ReadLine();
-        bool successY = int.TryParse(yPlaceString, out int yPlace);
-        bool successX = int.TryParse(xPlaceString, out int xPlace);
+        successY = int.TryParse(yPlaceString, out int yPlace);
+        successX = int.TryParse(xPlaceString, out int xPlace);
+        if (successY && successX)
+        {
+            if (p1)
+            {
+                if (board2[yPlace - 1, xPlace - 1] == 1)
+                {
+                    Console.WriteLine("HIT!");
+                }
+                else
+                {
+                    Console.WriteLine("Miss");
+                }
+            }
+            else
+            {
+                if (board1[yPlace - 1, xPlace - 1] == 1)
+                {
+                    Console.WriteLine("HIT!");
+                }
+                else
+                {
+                    Console.WriteLine("Miss");
+                }
+            }
+        }
     }
+}
+
+static bool IsAlive(bool p1, int[,] board1, int[,] board2, int height, int width)
+{
+    for (int y = 0; y < height; y++)
+    {
+        for (int x = 0; x < width; x++)
+        {
+            if (p1)
+            {
+                if (board1[y, x] == 1)
+                {
+                    return true;
+                }
+            }
+            else
+            {
+                if (board2[y, x] == 1)
+                {
+                    return true;
+                }
+            }
+        }
+    }
+    return false;
 }
 
 static int RandomBoardSquareY(int max)//Skapar ett slumpat värde som inte är mer än höjden på brädet
 {
     return Random.Shared.Next(max);
 }
+
 static int RandomBoardSquareX(int max)// skapar ett slupat värde som inte är mer än bredden på brädet
 {
     return Random.Shared.Next(max);
 }
-//bajs
+
 Start();
-Console.ReadLine();
